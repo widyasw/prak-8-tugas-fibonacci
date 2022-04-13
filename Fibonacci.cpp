8fa9fdfc5cd2f28c1d94c8c06820bc8c4fb88c61
widyasw <99782577+widyasw@users.noreply.github.com>
2022-04-13 13:26:13 +0700

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, f1 = 0, f2 = 1, berikutnya = 0;
+    // gunanya untuk mencari nilai yang diinginkan
+
+    cout << "Masukan Batas Deret Bilangan Fibonacci :  ";
+    cin >> n;
+	cout<<endl;
+    cout << "Deret Fibonacci: ";
+
+    for (int i = 1; i <= n; ++i)
+    {
+        // Mencetak dua deret bilangan fibonacci pertama.
+        if(i == 1)
+        {
+            cout << " " << f1<<" ";
+            continue;
+        }
+        if(i == 2)
+        {
+            cout << f2 << " ";
+            continue;
+        }
+        berikutnya = f1 + f2;
+        //yaitu rumus umum fibonacci 2 nilai sebelumnya dijumlahkan
+        f1 = f2;
+        f2 = berikutnya;
+         // Mencetak deret bilangan fibonacci berikutnya.
+        cout << berikutnya << " ";
+    }
+    return 0;
+}
